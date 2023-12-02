fc2160646e9aea49c3840dd270fed9c5cd68756f
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-12-02 14:39:50 +0530

    bodyParser and posting result using backend

---
+++ calculator.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const { num1, num2 } = req.body;
+  const result = Number(num1) + Number(num2);
+  res.send(`The sum of ${num1} and ${num2} is ${result}`);
+});
+
+app.listen(3000, () => {
+  console.log("Calculator backend server is running on port 3000");
+});
