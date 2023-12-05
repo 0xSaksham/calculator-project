600682429afa45a8973a7e2217e3bf8d86b2589d
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-12-05 16:58:56 +0530

    Add BMI calculator route and functionality

--- calculator.js
+++ calculator.js
@@ -8,11 +8,25 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmiCalculator", (req,res)=>{
+  res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
 app.post("/", (req, res) => {
   const { num1, num2 } = req.body;
   const result = Number(num1) + Number(num2);
   res.send(`The sum of ${num1} and ${num2} is ${result}`);
 });
+
+app.post("/bmiCalculator", (req, res) => {
+  
+  const weight = parseFloat(req.body.weight)
+  const height = parseFloat(req.body.height)
+
+  const result = weight / (height * height);
+  res.send(`Your BMI is ${10000*result}`);
+})
+
 
 app.listen(3000, () => {
   console.log("Calculator backend server is running on port 3000");
